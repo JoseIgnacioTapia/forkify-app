255f47229faee1c535a5dcb708aa758d9967490c
JoseIgnacioTapia <josignaciot@gmail.com>
2021-02-17 23:17:41 -0300

    Implementing Pagination - Determining the status of each page by number of pages

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,6 +2,7 @@ import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
+import paginationView from './views/paginationView';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
@@ -41,7 +42,10 @@ const controlSearchResults = async function () {
 
     // 3) Render results
     // resultsView.render(model.state.search.results);
-    resultsView.render(model.getSearchResultPage());
+    resultsView.render(model.getSearchResultPage(3));
+
+    // 4) Render initial pagination buttons
+    paginationView.render(model.state.search);
   } catch (err) {
     console.log(err);
   }
